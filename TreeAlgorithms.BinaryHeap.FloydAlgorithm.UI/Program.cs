88ea505ae8235f1921bb92c5dc23522ce7ef840d
petro kolosov <kolosovp94@gmail.com>
2020-09-11 13:03:38 +0300

    floyd algorithms implemented

---
+++ TreeAlgorithms.BinaryHeap.FloydAlgorithm.UI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TreeAlgorithms.BinaryHeap.FloydAlgorithm.Implementation;
+
+namespace TreeAlgorithms.BinaryHeap.FloydAlgorithm.UI
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var arr = new[] {1, 5, 4, 2, 7, 8, 9, 12, 15, 20, 31, 42, 2, 6};
+            var floydHeap = FloydBuildHeap.BuildMaxHeap(arr);
+            
+
+            while (!floydHeap.IsEmpty)
+            {
+                Console.Write(floydHeap.Pop() + " ");
+            }
+        }
+    }
+}
---
+++ TreeAlgorithms.BinaryHeap.FloydAlgorithm/Implementation/FloydBuildHeap.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using TreeAlgorithms.BinaryHeap.Implementations;
+
+namespace TreeAlgorithms.BinaryHeap.FloydAlgorithm.Implementation
+{
+    public class FloydBuildHeap : MaxHeap
+    {
+        public override int Pop()
+        {
+            var result = HeapBase[0];
+            HeapBase[0] = HeapBase.Last();
+            HeapBase.RemoveAt(Count - 1);
+            FloydRestoreHeapProperty();
+            return result;
+        }
+
+        private void FloydRestoreHeapProperty()
+        {
+            for (var i = Count / 2; i >= 0; i--)
+                CalculateDown(i);
+        }
+        
+        public static FloydBuildHeap BuildMaxHeap(params int[] array)
+        {
+            var heap = new FloydBuildHeap();
+
+            foreach (var number in array)
+                heap.Push(number);
+
+            return heap;
+        }
+    }
+}
