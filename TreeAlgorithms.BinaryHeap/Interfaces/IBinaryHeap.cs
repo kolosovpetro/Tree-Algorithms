bea2d1844a44678ce34f6f313aaed41364e6c7b1
petro kolosov <kolosovp94@gmail.com>
2020-09-11 12:34:46 +0300

    binary heap migrated

---
+++ TreeAlgorithms.BinaryHeap/Interfaces/IBinaryHeap.cs
@@ -0,0 +1,23 @@
+﻿namespace TreeAlgorithms.BinaryHeap.Interfaces
+{
+    public interface IBinaryHeap
+    {
+        int Count { get; }
+        bool IsEmpty { get; }
+        int ParentIndex(int childIndex);
+        int LeftChildIndex(int parentIndex);
+        int RightChildIndex(int parentIndex);
+        bool HasLeft(int parentIndex);
+        bool HasRight(int parentIndex);
+        int GetParent(int childIndex);
+        int GetLeftChild(int parentIndex);
+        int GetRightChild(int parentIndex);
+        void Push(int element);
+        void PushRange(params int[] array);
+        int Pop();
+        int Peek();
+        void CalculateUp(int elementIndex);
+        void CalculateDown(int elementIndex);
+        void PrintHeap();
+    }
+}
---
+++ TreeAlgorithms.Tests/BinaryHeapTests/MaxHeapTests/MaxHeapInitializationTest.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TreeAlgorithms.BinaryHeap.Implementations;
+using TreeAlgorithms.BinaryHeap.Interfaces;
+
+namespace TreeAlgorithms.Tests.BinaryHeapTests.MaxHeapTests
+{
+    [TestFixture]
+    public class MaxHeapInitializationTest
+    {
+        [Test]
+        public void Max_Heap_Initialization_Test()
+        {
+            IBinaryHeap heap = new MaxHeap();
+            heap.IsEmpty.Should().BeTrue();
+            heap.Count.Should().Be(0);
+        }
+    }
+}
