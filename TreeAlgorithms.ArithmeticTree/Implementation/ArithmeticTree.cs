542cabc1e354d7ccd9795acc9031b241a3ef9437
petro kolosov <kolosovp94@gmail.com>
2020-09-11 12:58:54 +0300

    arithmetic tree implemented

---
+++ TreeAlgorithms.ArithmeticTree/Implementation/ArithmeticTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using TreeAlgorithms.BinaryTree.Implementations;
+using TreeAlgorithms.BinaryTree.Interfaces;
+
+namespace TreeAlgorithms.ArithmeticTree.Implementation
+{
+    public static class ArithmeticTree
+    {
+        public static IBinaryTree<char> BuildArithmeticTree(string postfix)
+        {
+            var stack = new Stack<IBinaryTree<char>>();
+
+            foreach (var input in postfix)
+            {
+                if (char.IsDigit(input))
+                {
+                    var tree = new BinaryTree<char>(input);
+                    stack.Push(tree);
+                }
+                
+                else if (!char.IsDigit(input))
+                {
+                    var tree1 = stack.Pop();
+                    var tree2 = stack.Pop();
+                    var tree = new BinaryTree<char>(input);
+                    tree.AddLeft(tree2);
+                    tree.AddRight(tree1);
+                    stack.Push(tree);
+                }
+            }
+
+            return stack.Pop();
+        }
+
+        public static void PrintInfixExpression(IBinaryTree<char> tree)
+        {
+            if (tree.HasLeft)
+            {
+                Console.Write("( ");
+                PrintInfixExpression(tree.Left);
+            }
+
+            Console.Write(tree.Data + " ");
+
+            if (tree.HasRight)
+            {
+                PrintInfixExpression(tree.Right);
+                Console.Write(" )");
+            }
+        }
+    }
+}
---
+++ TreeAlgorithms.Tests/ArithmeticTreeTests/ArithmeticTreeTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TreeAlgorithms.ShuntingYardAlgorithm.Implementation;
+
+namespace TreeAlgorithms.Tests.ArithmeticTreeTests
+{
+    [TestFixture]
+    public class ArithmeticTreeTests
+    {
+        [Test]
+        public void Tree_Test()
+        {
+            var initialEquation = "3+2*4/5";
+            var checkPostfix = ShuntingYardMethod.PostfixString(initialEquation);
+            checkPostfix.Should().Be("324*5/+");
+            var arithmeticTree = ArithmeticTree.Implementation.ArithmeticTree.BuildArithmeticTree(checkPostfix);
+            arithmeticTree.Root.Data.Should().Be('+');
+        }
+    }
+}
