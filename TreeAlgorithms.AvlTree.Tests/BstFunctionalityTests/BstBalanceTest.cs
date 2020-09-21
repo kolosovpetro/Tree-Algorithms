3ef72ebaf50fa62eee9a5beb7b45fd043858e05d
razumovskiy <kolosovp94@gmail.com>
2020-09-21 21:50:20 +0300

    avl tests written:

---
+++ TreeAlgorithms.AvlTree.Tests/BstFunctionalityTests/BstBalanceTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TreeAlgorithms.AvlTree.Interfaces;
+
+namespace TreeAlgorithms.AvlTree.Tests.BstFunctionalityTests
+{
+    [TestFixture]
+    public class BstBalanceTest
+    {
+        [Test]
+        public void Bst_Balance_Test_1()
+        {
+            IAvlTree tree = new Implementations.AvlTree(1);
+            var v2 = tree.BstInsert(2);
+            var v3 = tree.BstInsert(3);
+            var v4 = tree.BstInsert(4);
+
+            tree.Balance.Should().Be(3);
+            v2.Balance.Should().Be(2);
+            v3.Balance.Should().Be(1);
+            v4.Balance.Should().Be(0);
+        }
+
+        [Test]
+        public void Bst_Balance_Test_2()
+        {
+            IAvlTree tree = new Implementations.AvlTree(10);
+            var v2 = tree.BstInsert(11);
+            var v3 = tree.BstInsert(9);
+            var v4 = tree.BstInsert(6);
+            var v5 = tree.BstInsert(8);
+
+            tree.Balance.Should().Be(1);
+        }
+    }
+}
---
+++ TreeAlgorithms.AvlTree.Tests/BstFunctionalityTests/BstDeleteTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TreeAlgorithms.AvlTree.Interfaces;
+
+namespace TreeAlgorithms.AvlTree.Tests.BstFunctionalityTests
+{
+    [TestFixture]
+    public class BstDeleteTest
+    {
+        [Test]
+        public void Bst_Delete_Test()
+        {
+            IAvlTree tree = new Implementations.AvlTree(50);
+            
+            // sub tree 1
+            tree.BstInsert(72);
+            tree.BstInsert(54);
+            tree.BstInsert(76);
+            tree.BstInsert(67);
+            
+            // sub tree 2
+            tree.BstInsert(17);
+            tree.BstInsert(12);
+            tree.BstInsert(23);
+            tree.BstInsert(9);
+            tree.BstInsert(14);
+            tree.BstInsert(19);
+
+            tree.BstDelete(tree.Right).Key.Should().Be(76);
+            tree.Count().Should().Be(10);
+            tree.Right.Key.Should().Be(76);
+        }
+    }
+}
